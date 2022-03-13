a3d07b856d886e541183788ce7b096482db74bde
steve <steve.huckle@gmail.com>
2022-03-13 21:38:40 +0000

    add functional keyboard

--- src/components/App.js
+++ src/components/App.js
@@ -6,38 +6,73 @@ export const App = () => {
   const [calculation, setCalculation] = useState("")
   const [answer, setAnswer] = useState("");
 
-  const handleChange = (event) => {
+  const handleClear = () => {
+    setCalculation("");
     setAnswer("");
-    setCalculation(event.target.value);
   } 
 
-  const handleSubmit = (event) => {
+  const handleBackspace = () => {
+    const thisCalc = calculation.slice(0, -1)
+    setCalculation(thisCalc);
+  }
+  
+  const handleBrackets = () => {
+    const thisCalc = "(" + calculation + ")";
+    setCalculation(thisCalc);
+  } 
+
+  const handleOperand = (event) => {
+    const thisCalc = calculation + event.target.value
+    setCalculation(thisCalc);
+  } 
+
+  const handleOperator = (event) => {
+    const thisCalc = calculation + " " + event.target.value + " "
+    setCalculation(thisCalc);
+  } 
+
+  const handleCalculate = (event) => {
     event.preventDefault();
     const answer = calculate(calculation);
-    //console.log('client answer', calculation, answer)
-    if ( answer ) {
-      setAnswer(answer)
-    } else {
-      setAnswer("Invalid calc")
-    }
+    setCalculation("");
+    setAnswer(answer)
   }
 
   return (
-    <>     
-      <h2>Calculator</h2> 
-      <form onSubmit={handleSubmit}>
-        <label id="calc" htmlFor="calc">Calculation: </label>
+    <main>   
         <input
+          readOnly
           type="text"
-          id="calc"
+          id="display"
           name="calc"
-          onChange={handleChange}
-          value={calculation}
+          value={calculation ? calculation : answer}
         />
-        &nbsp;
-        <button type="submit">Calculate</button>
-      </form>
-      <p>Answer: {answer}</p>      
-    </>
+        <div id="keyboard">
+          <input type="button" value="AC" onClick={handleClear}/>
+          <input type="button" value="()" onClick={handleBrackets}/>
+          <input type="button" value="^" onClick={handleOperator}/>
+          <input type="button" value="/" onClick={handleOperator}/>
+
+          <input type="button" value="7" onClick={handleOperand}/>
+          <input type="button" value="8" onClick={handleOperand}/>
+          <input type="button" value="9" onClick={handleOperand}/>
+          <input type="button" value="*" onClick={handleOperator}/>
+
+          <input type="button" value="4" onClick={handleOperand}/>
+          <input type="button" value="5" onClick={handleOperand}/>
+          <input type="button" value="6" onClick={handleOperand}/>
+          <input type="button" value="-" onClick={handleOperator}/>
+
+          <input type="button" value="1" onClick={handleOperand}/>
+          <input type="button" value="2" onClick={handleOperand}/>
+          <input type="button" value="3" onClick={handleOperand}/>
+          <input type="button" value="+" onClick={handleOperator}/>
+
+          <input type="button" value="0" onClick={handleOperand}/>
+          <input type="button" value="." onClick={handleOperand}/>
+          <input type="button" value="&#9003;" onClick={handleBackspace}/>
+          <input type="button" value="=" onClick={handleCalculate}/>
+        </div>   
+    </main>
   );
 }
